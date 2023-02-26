097bb4d4f6022b4d32f1e5d4a225e4007c555b7c
PolinaSuvorova <polina.tatara@gmail.com>
2023-02-26 22:41:35 +0300

    Lesson 9 26.02.2023 Создание класса на основе Path. Создание класса вывода через паттерн стратегия

--- test/com/urise/webapp/storage/RunTests.java
+++ test/com/urise/webapp/storage/RunTests.java
@@ -9,7 +9,8 @@ import org.junit.runners.Suite;
         SortedArrayStorageTest.class,
         MapStorageTest.class,
         ListStorageTest.class,
-        ObjectStreamStorageTest.class })
+        ObjectStreamFileStorageTest.class,
+        ObjectStreamPathStorageTest.class})
 public class RunTests {
 
 }
