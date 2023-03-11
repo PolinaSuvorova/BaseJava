cdef5aa35ce9a2395d980c850dec1dca7fb7786b
PolinaSuvorova <polina.tatara@gmail.com>
2023-03-11 18:54:49 +0300

    Lesson 12 Работа с потоками

---
+++ src/main/java/com/urise/webapp/MainStream.java
@@ -0,0 +1,34 @@
+package com.urise.webapp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainStream {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{2, 1, 1, 4, 1, 5};
+        System.out.println("minValue = " + minValue(numbers));
+
+        List<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(3);
+        list.add(1);
+        System.out.println(oddOrEven(list));
+    }
+
+    private static int minValue(int[] values) {
+        return Arrays.stream(values)
+                .sorted()
+                .distinct()
+                .reduce(0, (left, right) ->
+                        left * 10 + right);
+    }
+
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        boolean isOddNum = integers.stream().anyMatch(n -> n % 2 != 0);
+        return integers.stream().filter(integer ->
+                        ((integer % 2 != 0 && !isOddNum) || (integer % 2 == 0 && isOddNum)))
+                .toList();
+
+    }
+}
