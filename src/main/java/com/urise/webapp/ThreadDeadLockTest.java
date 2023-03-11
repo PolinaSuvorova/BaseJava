7b08a26ee417db21d65db57536f74ae9409651ef
PolinaSuvorova <polina.tatara@gmail.com>
2023-03-11 19:00:38 +0300

    Lesson 12 Работа с потоками

--- src/main/java/com/urise/webapp/ThreadDeadLockTest.java
+++ src/main/java/com/urise/webapp/ThreadDeadLockTest.java
@@ -6,23 +6,23 @@ public class ThreadDeadLockTest {
 
     public static void main(String[] args) {
         Thread thread1 = new Thread(() -> {
-            synchronized (lock1) {
-                System.out.println("Поток 1 блокировка lock1 ");
-                synchronized (lock2) {
-                    System.out.println("Поток 1 блокировка lock1 lock2");
-                }
-            }
+            lock(lock1,lock2);
         });
         Thread thread2 = new Thread(() -> {
-            synchronized (lock2) {
-                System.out.println("Поток 2 блокировка lock2");
-                synchronized (lock1) {
-                    System.out.println("Поток 2 блокировка lock2 lock1");
-                }
-            }
+            lock(lock2,lock1);
         });
         thread1.start();
         thread2.start();
+    }
+
+    private static void lock(Object lock1,Object lock2) {
+        synchronized (lock1) {
+            System.out.println("блокировка" + lock1);
+            synchronized (lock2) {
+                System.out.println("блокировка " + lock1 + " и " + lock2);
+
+            }
+        }
     }
 
 }
