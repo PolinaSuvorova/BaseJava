ecd54b6cd053c0690c9e77862eb7582333cae748
PolinaSuvorova <polina.tatara@gmail.com>
2023-03-27 22:02:01 +0300

    Lesson 15  HW15 сервелет

--- src/main/java/com/urise/webapp/web/ResumeServlet.java
+++ src/main/java/com/urise/webapp/web/ResumeServlet.java
@@ -1,8 +1,15 @@
 package com.urise.webapp.web;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.util.Config;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
     @Override
@@ -10,8 +17,27 @@ public class ResumeServlet extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null? "Hello":"Helloy " + name);
+        //     String name = request.getParameter("name");
+        //     response.getWriter().write(name == null? "Hello":"Helloy " + name);
+        PrintWriter writer = response.getWriter();
+        writer.println(
+                "<table>\n" +
+                "<tbody>\n" +
+                "<tr>\n" +
+                "<th>UUID</th><th>Full Name</th>\n" +
+                "</tr>");
+        List<Resume> list = Config.getInstance()
+                .getSqlStorage()
+                .getAllSorted();
+        for (Resume r : list) {
+            writer.println("<tr>");
+            writer.println("<td>" + r.getUuid() + "</td>");
+            writer.println("<td>" + r.getFullName() + "</td>");
+            writer.println("</tr>");
+        }
+        writer.println(
+                "</tbody\n" +
+                "</table>");
     }
 
     @Override
